124d933b676f2a3fe94303f95ef5c3e1350be3de
泽泽兮 <yandiwu97@163.com>
2020-12-09 19:54:14 +0800

    jack

---
+++ src/views/login/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    登录页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: '', 
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
